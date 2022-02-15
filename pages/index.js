75cd322c919d2689d76ed0d3637571d6c0638dec
ADOUKO Franck Venance <fvadouko@live.fr>
2022-02-15 17:54:36 +0000

    toaster and loader

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Loader from "../components/Loader";
+import toast from "react-hot-toast";
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return (
+    <div>
+      <Loader show={true} />
+      <button onClick={() => toast.success("hello toast!")}>Toast Me</button>
+    </div>
+  );
 }

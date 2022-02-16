4c4e2b953923aef11542cc2ff23307336c16356f
ADOUKO Franck Venance <fvadouko@live.fr>
2022-02-16 12:07:51 +0000

    set up context for the user and his username;

--- pages/enter.js
+++ pages/enter.js
@@ -1,10 +1,11 @@
+import { useContext } from "react";
 import SignInButton from "../components/SignInButton";
 import SignOutButton from "../components/SignOutButton";
 import UsernameForm from "../components/UsernameForm";
+import { UserContext } from "../lib/context";
 
 export default function Enter(props) {
-  const user = null;
-  const username = null;
+  const { user, username } = useContext(UserContext);
 
   // 1. user signed out <SignInButton />
   // 2. user signed in, but missing username <UsernameForm />

104e5f4e0667fe337daf845d4015c84b868e9870
ADOUKO Franck Venance <fvadouko@live.fr>
2022-02-16 14:17:29 +0000

    Display user profile page.

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,7 +1,39 @@
-import React from "react";
+import { getUserWithUsername, postToJSON } from "../../lib/firebase";
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed";
+import SignOutButton from "../../components/SignOutButton";
 
-const PostPage = () => {
-  return <div>PostPage</div>;
-};
+export async function getServerSideProps({ query }) {
+  console.log("query", query);
+  const { username } = query;
 
-export default PostPage;
+  const userDoc = await getUserWithUsername(username);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .limit(5);
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
+  return (
+    <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+      <SignOutButton />
+    </main>
+  );
+}
